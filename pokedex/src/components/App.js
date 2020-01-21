ff2f309646257a48b16e302e96cacec38b81ee5d
syedishmam <syedishmam17@gmail.com>
2020-01-21 18:05:28 -0500

    continue bp

--- pokedex/src/components/App.js
+++ pokedex/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Profile from './pokemon_profile/Profile';
+
 class App extends React.Component {
     render() {
         return (
-            <div>App</div>
+            <Profile />
         )
     }
 }
---
+++ pokedex/src/components/pokemon_profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/Profile.css';
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <div className='profileContianer'>
+                <p>Profile</p>
+            </div>
+        )
+    }
+}
+
+export default Profile;
