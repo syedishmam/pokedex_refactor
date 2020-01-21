adae184e52575b5f24f5ba9f1314da4491b353f0
syedishmam <syedishmam17@gmail.com>
2020-01-21 18:49:30 -0500

    general info boilerplate

--- pokedex/src/components/pokemon_profile/Profile.js
+++ pokedex/src/components/pokemon_profile/Profile.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import GenInfo from './gen_info_card/GenInfo.js';
+
 import './styles/Profile.css';
 
 class Profile extends React.Component {
     render() {
         return (
             <div className="profileContainer">
-                <p>Profile</p>
+                <GenInfo />
             </div>
         )
     }
---
+++ pokedex/src/components/pokemon_profile/gen_info_card/GenInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/GenInfo.css';
+
+class GenInfo extends React.Component {
+    render() {
+        return (
+            <div id="generalInfoContainer">
+                <p>General Info</p>
+            </div>
+        )
+    }
+}
+
+export default GenInfo;
