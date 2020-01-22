b6f2419ff807e23aa89a4bcb44066740da7f0ba5
syedishmam <syedishmam17@gmail.com>
2020-01-21 19:05:38 -0500

    detailed info bp

--- pokedex/src/components/pokemon_profile/Profile.js
+++ pokedex/src/components/pokemon_profile/Profile.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import GenInfo from './gen_info_card/GenInfo.js';
+import GenInfo from './gen_card/GenInfo.js';
 
 import './styles/Profile.css';
+import DetailedInfo from './detailed_card/DetailedInfo.js';
 
 class Profile extends React.Component {
     render() {
         return (
             <div className="profileContainer">
                 <GenInfo />
+                <DetailedInfo/>
             </div>
         )
     }
---
+++ pokedex/src/components/pokemon_profile/detailed_card/DetailedInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/DetailedInfo.css';
+
+class DetailedInfo extends React.Component {
+    render() {
+        return (
+            <div id="detailedInfoContainer">
+                <p>Detailed Info</p>
+            </div>
+        )
+    }
+}
+
+export default DetailedInfo;
