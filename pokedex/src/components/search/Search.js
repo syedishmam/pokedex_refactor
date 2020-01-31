e29a751a6ebf4126e373e65bb85ca12bd7cc8d48
syedishmam <syedishmam17@gmail.com>
2020-01-31 18:09:30 -0500

    Create Search component

---
+++ pokedex/src/components/search/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import pokeApi from '../../api/pokeApi.js';
+
+class Search extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>Search</p>
+            </div>
+        )
+    }
+}
+
+export default Search;
