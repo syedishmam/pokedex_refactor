e1c6d74afbf2ed02e283e30f8f01660d0a838454
syedishmam <syedishmam17@gmail.com>
2020-02-01 16:12:24 -0800

    Create pokeapi search funcitonality

--- pokedex/src/components/search/Search.js
+++ pokedex/src/components/search/Search.js
@@ -3,10 +3,30 @@ import React from 'react';
 import pokeApi from '../../api/pokeApi.js';
 
 class Search extends React.Component {
+
+    state = {searchQuery: ''};
+
+    fetchPokemon = async (pokemon) => {
+        const response = await pokeApi.get('pokemon/' + pokemon)
+        console.log(response.data.species);
+    }
+
+    onEnterKeyPress = (event) => {
+        if(event.key === 'Enter') {
+            this.fetchPokemon(this.state.searchQuery);
+        }
+    }
+
+    updateSearchQuery = (event) => {
+        this.setState({ searchQuery: event.target.value });
+        console.log(this.state.searchQuery);
+    }
+
     render() {
         return(
             <div>
-                <input type="text" />
+                <input type="text" value={this.state.searchQuery} onChange={this.updateSearchQuery} onKeyPress={this.onEnterKeyPress}/>
+                <button onClick={() => this.fetchPokemon(this.state.searchQuery)}>Submit</button>
             </div>
         )
     }
