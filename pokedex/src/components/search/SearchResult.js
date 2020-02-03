32e1f36068cedb2b36ad32dc5c04338772e33d7a
syedishmam <syedishmam17@gmail.com>
2020-02-03 17:48:47 -0500

    Insert searched pokemon name, add styling

--- pokedex/src/components/search/SearchResult.js
+++ pokedex/src/components/search/SearchResult.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import './styles/SearchResult.css';
+
 class SearchResult extends React.Component {
     render() {
         return (
-            <div>
+            <div id="searchResultContainer">
                 <img src={this.props.search.sprites.front_default} alt="pokemon sprite" />
+                <p>{this.props.search.species.name}</p>
             </div>
         )
     }
