3bd05addbc7c91f7dbeac6addd265b61484b7805
syedishmam <syedishmam17@gmail.com>
2020-02-03 18:01:22 -0500

    Link back button to /search

--- pokedex/src/components/pokemon_profile/gen_card/Header.js
+++ pokedex/src/components/pokemon_profile/gen_card/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
 import './styles/Header.css';
 
 class Header extends React.Component {
     render() {
         return (
             <div className="headerContainer">
-                <button className="backButton">Back</button>
+                <Link className="backButton" to={'/search'}>Back</Link>
                 <button className="favoriteButton">Favorite</button>
             </div>
         )
