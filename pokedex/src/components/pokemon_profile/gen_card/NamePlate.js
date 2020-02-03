4f5c9beb2cee95848cd03204013b88bf16f8ebb8
syedishmam <syedishmam17@gmail.com>
2020-02-03 18:24:58 -0500

    Bug: Cannot read property  when /profile refreshed

--- pokedex/src/components/pokemon_profile/gen_card/NamePlate.js
+++ pokedex/src/components/pokemon_profile/gen_card/NamePlate.js
@@ -1,22 +1,45 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Cell from './Cell.js';
 
 import './styles/NamePlate.css';
 
 class NamePlate extends React.Component {
+
+    renderPokemonProfile() {
+        if(this.props.pokemonPicture || this.props.pokemonName) {
+            return(
+                <div>
+                    <h1 className="pokemonName">{this.props.pokemonName}</h1>
+                    <div className="cells">
+                        <Cell />
+                        <Cell />
+                    </div>
+                    <img id="pokemonSprite" src={this.props.pokemonPicture} alt="Pokemon Sprite" />
+                </div>
+            )
+        } else {
+            return (
+                <p>Pokemon Profile</p>
+            )
+        }
+    }
+
     render() {
         return (
             <div className="namePlateContainer">
-                <h1 className="pokemonName">Pokemon Name</h1>
-                <div className="cells">
-                    <Cell />
-                    <Cell />
-                </div>
-                <img id="pokemonSprite" src="https://via.placeholder.com/200" alt="Pokemon Sprite" />
+                {this.renderPokemonProfile()}
             </div>
         )
     }
 }
 
-export default NamePlate;
+const mapStateToProps = (state) => {
+    return {
+        pokemonPicture: state.pokemonData.data.sprites.front_default,
+        pokemonName: state.pokemonData.data.species.name
+    }
+}
+
+export default connect(mapStateToProps)(NamePlate);
