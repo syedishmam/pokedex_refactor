e15713a8531aa23677b260d84b30e3a6ffebd776
syedishmam <syedishmam17@gmail.com>
2020-02-05 18:59:08 -0500

    Bug Fix: Refresh page causes crash

--- pokedex/src/components/pokemon_profile/Profile.js
+++ pokedex/src/components/pokemon_profile/Profile.js
@@ -2,19 +2,45 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import GenInfo from './gen_card/GenInfoContainer.js';
+import pokeApi from '../../api/pokeApi.js'
+import {storePokemonData} from '../../actions/index.js'
 
 import './styles/Profile.css';
 import DetailedInfo from './detailed_card/DetailedInfo.js';
 
 class Profile extends React.Component {
+
+    fetchPokemonIfNoData = async (pokemon) => {
+        const response = await pokeApi.get('pokemon/' + pokemon);
+        this.props.storePokemonData(response.data);
+        console.log(response.data)
+    }
+
+    renderProfile() {
+        if(this.props.data) {
+            return (
+                <div>
+                    <GenInfo />
+                    <DetailedInfo/>
+                </div>
+            ) 
+        } else {
+            this.fetchPokemonIfNoData(document.URL.substring(30));
+            
+        }
+    }
+
     render() {
         return (
             <div className="profileContainer">
-                <GenInfo />
-                <DetailedInfo/>
+                {this.renderProfile()}
             </div>
         )
     }
 }
 
-export default connect()(Profile);
+const mapStateToProps = (state) => {
+    return {data: state.pokemonData.data}
+}
+
+export default connect(mapStateToProps, {storePokemonData})(Profile);
