937882ad3b6515ae8211a5abb2f13872a8d8d284
syedishmam <syedishmam17@gmail.com>
2020-02-06 17:13:03 -0500

    Display types for pokemon

--- pokedex/src/components/search/SearchResult.js
+++ pokedex/src/components/search/SearchResult.js
@@ -7,8 +7,28 @@ import './styles/SearchResult.css';
 class SearchResult extends React.Component {
 
     capitalizePokemonName(pokemon) {
-        const capitalize = pokemon.substring(0, 1).toUpperCase().concat(pokemon.substring(1));
+        const capitalize = this.props.search.species.name.substring(0, 1).toUpperCase().concat(pokemon.substring(1));
         return capitalize;
+    }
+
+    //Displays up to 2 types for selected pokemon 
+    renderTypes() {
+        const pokemonTypesArray = [];
+        for(let i = 0; i < this.props.pokemonTypes.length; i++) {
+            pokemonTypesArray.push(this.props.pokemonTypes[i].type.name);
+        }
+        if(pokemonTypesArray.length === 2) {
+            return (
+                <div>
+                    <p>{pokemonTypesArray[0]}</p>
+                    <p>{pokemonTypesArray[1]}</p>
+                </div>
+            )
+        } else {
+            return (
+                <p>{pokemonTypesArray[0]}</p>
+            )
+        }
     }
 
     render() {
@@ -17,10 +37,7 @@ class SearchResult extends React.Component {
                 <img id="pokemonImage" src={this.props.search.sprites.front_default} alt="pokemon sprite" />
                 <div id="infoCard">
                     <h3 id="pokemonName">{this.capitalizePokemonName(this.props.search.species.name)}</h3>
-                    <div>
-                        <p>Type</p>
-                        <p>Type</p>
-                    </div>
+                    {this.renderTypes()}
                 </div>
             </Link>
         )
@@ -28,7 +45,7 @@ class SearchResult extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return {pokemonName: state.pokemonData.data.species.name}
+    return {pokemonName: state.pokemonData.data.species.name, pokemonTypes: state.pokemonData.data.types}
 }
 
 export default connect(mapStateToProps)(SearchResult);
