6b215d5988fe4725b48a794fcf6b16f8fb82c4e7
syedishmam <syedishmam17@gmail.com>
2020-02-06 16:47:54 -0500

    infoCard styling and text

--- pokedex/src/components/search/SearchResult.js
+++ pokedex/src/components/search/SearchResult.js
@@ -5,11 +5,23 @@ import {connect} from 'react-redux';
 import './styles/SearchResult.css';
 
 class SearchResult extends React.Component {
+
+    capitalizePokemonName(pokemon) {
+        const capitalize = pokemon.substring(0, 1).toUpperCase().concat(pokemon.substring(1));
+        return capitalize;
+    }
+
     render() {
         return (
             <Link to={`/profile/${this.props.pokemonName}`} id="searchResultContainer">
                 <img id="pokemonImage" src={this.props.search.sprites.front_default} alt="pokemon sprite" />
-                <p>{this.props.search.species.name}</p>
+                <div id="infoCard">
+                    <h3 id="pokemonName">{this.capitalizePokemonName(this.props.search.species.name)}</h3>
+                    <div>
+                        <p>Type</p>
+                        <p>Type</p>
+                    </div>
+                </div>
             </Link>
         )
     }
