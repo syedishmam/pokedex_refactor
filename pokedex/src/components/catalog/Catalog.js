87e600d58dd1d18900820e372b1f653a03c535a9
syedishmam <syedishmam17@gmail.com>
2020-02-18 16:29:21 -0500

    Boilerplate

--- pokedex/src/components/catalog/Catalog.js
+++ pokedex/src/components/catalog/Catalog.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import Search from './search/Search.js'
 
+import './styles/Catalog.css';
+
 class Catalog extends React.Component {
     render() {
         return (
-            <div>
+            <div id="catalogContainer">
+                <h1>Pokedex</h1>
                 <Search />
             </div>
         )
