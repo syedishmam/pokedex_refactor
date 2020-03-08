1c618139f3602978e0f69f2fc6552d5d08bf8ad3
syedishmam <syedishmam17@gmail.com>
2020-03-08 19:29:20 -0400

    Export and comments

--- pokedex/src/api/apiFunctions.js
+++ pokedex/src/api/apiFunctions.js
@@ -5,16 +5,19 @@ export const fetchPokemon = async (pokemon, dataStore) => {
     const response = await pokeApi.get('pokemon/' + pokemon);
     const speciesData = await pokeApi.get('pokemon-species/' + response.data.id);
     dataStore(response.data, speciesData.data);
-    console.log(speciesData.data);
+    console.log(response.data);
+    return response.data;
 }
 
 //Rearrange array for main types to be displayed first and to have theme color reflect accordingly
-export const orderTypesByImportance = (types) => {
+//Stores types array in redux store
+export const orderTypesByImportance = (types, typesStore) => {
     if(types[1] === "Water" || types[1] === "Fire" || types[1] === "Grass" || types[1] === "Ground"  || types[1] === "Electric") {
         const temp = types[0];
         types[0] = types[1];
         types[1] = temp;
     }
+    typesStore(types);
     return types;
 }
 
