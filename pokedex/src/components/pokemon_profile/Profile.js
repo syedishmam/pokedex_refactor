012a12a0aad5b9ccfdf315eaeb873c851d9798cd
syedishmam <syedishmam17@gmail.com>
2020-03-20 15:02:33 -0400

    Add getEngDesc to performApiFunctions

--- pokedex/src/components/pokemon_profile/Profile.js
+++ pokedex/src/components/pokemon_profile/Profile.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import GenInfo from './gen_card/GenInfo.js';
-import {storePokemonData, storePokemonTypes, changeTheme} from '../../actions/index.js';
-import {fetchPokemon, orderTypesByImportance, updateTheme} from '../../api/apiFunctions.js';
+import {storePokemonData, storePokemonTypes, storePokemonDescEnglish, changeTheme} from '../../actions/index.js';
+import {fetchPokemon, orderTypesByImportance, updateTheme, getEnglishPokemonDescription} from '../../api/apiFunctions.js';
 
 import './styles/Profile.css';
 import DetailedInfo from './detailed_card/DetailedInfo.js';
@@ -14,6 +14,7 @@ class Profile extends React.Component {
         const response = await fetchPokemon(document.URL.substring(30), this.props.storePokemonData);
         const orderedTypes = orderTypesByImportance(response.types, this.props.storePokemonTypes);
         updateTheme(orderedTypes, this.props.changeTheme);
+        getEnglishPokemonDescription(this.props.pokemonDescs, this.props.storePokemonDescEnglish);
     }
 
     renderProfile() {
@@ -43,8 +44,9 @@ class Profile extends React.Component {
 const mapStateToProps = (state) => {
     return {
         pokemonData: state.pokemonData.data,
+        pokemonDescs: state.pokemonData.speciesData.flavor_text_entries,
         themeColor: state.theme.theme
     }
 }
 
-export default connect(mapStateToProps, {storePokemonData, storePokemonTypes, changeTheme})(Profile);
+export default connect(mapStateToProps, {storePokemonData, storePokemonTypes, storePokemonDescEnglish, changeTheme})(Profile);
