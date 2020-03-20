34f7a70e50383f82f219f4f0ba2ed0a6d0962453
syedishmam <syedishmam17@gmail.com>
2020-03-20 13:52:43 -0400

    Nav bar styling and logic

--- pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/DetailedInfoNav.js
+++ pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/DetailedInfoNav.js
@@ -5,13 +5,69 @@ import {displayAbout, displayBaseStats, displayEvolution, displayMoves} from '..
 import './styles/DetailedInfoNav.css';
 
 class DetailedInfoNav extends React.Component {
+
+    state = {
+        abtBorder: {borderBottom: "solid 3px gray"},
+        statBorder: {borderBottom: "solid 3px gray"},
+        evoBorder: {borderBottom: "solid 3px gray"},
+        moveBorder: {borderBottom: "solid 3px gray"}
+    }
+
+    navButtonClick(tabName) {
+        switch(tabName) {
+            case "about":
+                this.props.displayAbout('about');
+                this.setState({
+                    abtBorder: {borderBottom: "solid 3px blue"},
+                    statBorder: {borderBottom: "solid 3px gray"},
+                    evoBorder: {borderBottom: "solid 3px gray"},
+                    moveBorder: {borderBottom: "solid 3px gray"}
+                });
+                break;
+
+            case "base_stats":
+                this.props.displayAbout('base_stats');
+                this.setState({
+                    abtBorder: {borderBottom: "solid 3px gray"},
+                    statBorder: {borderBottom: "solid 3px blue"},
+                    evoBorder: {borderBottom: "solid 3px gray"},
+                    moveBorder: {borderBottom: "solid 3px gray"}
+                });
+                break;
+
+            case "evolution":
+                this.props.displayAbout('evolution');
+                this.setState({
+                    abtBorder: {borderBottom: "solid 3px gray"},
+                    statBorder: {borderBottom: "solid 3px gray"},
+                    evoBorder: {borderBottom: "solid 3px blue"},
+                    moveBorder: {borderBottom: "solid 3px gray"}
+                });
+                break;
+
+            case "moves":
+                this.props.displayAbout('moves');
+                this.setState({
+                    abtBorder: {borderBottom: "solid 3px gray"},
+                    statBorder: {borderBottom: "solid 3px gray"},
+                    evoBorder: {borderBottom: "solid 3px gray"},
+                    moveBorder: {borderBottom: "solid 3px blue"}
+                });
+                break;
+
+            default: 
+                return
+        }
+    }
+
     render() {
+
         return (
             <div id="detailedInfoNavBarContainer">
-                <button className="detailedInfoNavButton" onClick={() => this.props.displayAbout('about')}>About</button>
-                <button className="detailedInfoNavButton" onClick={() => this.props.displayBaseStats('base_stats')}>Base Stats</button>
-                <button className="detailedInfoNavButton" onClick={() => this.props.displayEvolution('evolution')}>Evolution</button>
-                <button className="detailedInfoNavButton" onClick={() => this.props.displayMoves('moves')}>Moves</button>
+                <button className="detailedInfoNavButton" style={this.state.abtBorder} onClick={() => this.navButtonClick('about')}>About</button>
+                <button className="detailedInfoNavButton" style={this.state.statBorder} onClick={() => this.navButtonClick('base_stats')}>Base Stats</button>
+                <button className="detailedInfoNavButton" style={this.state.evoBorder} onClick={() => this.navButtonClick('evolution')}>Evolution</button>
+                <button className="detailedInfoNavButton" style={this.state.moveBorder} onClick={() => this.navButtonClick('moves')}>Moves</button>
             </div>
         )
     }
