9150f41fd4025545268527d537da3d323448a977
syedishmam <syedishmam17@gmail.com>
2020-03-20 15:03:06 -0400

    Display pokemon desc from redux store

--- pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/about_tab/About.js
+++ pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/about_tab/About.js
@@ -1,29 +1,21 @@
 import React from 'react';
-
-import PokemonDescription from './PokemonDescription.js';
-import Measurements from './Measurements.js';
+import {connect} from 'react-redux';
 
 class About extends React.Component {
 
     render() {
         return (
             <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore 
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat. Duis aute irure dolor in reprehenderit in 
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                    qui officia deserunt mollit anim id est laborum.
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore 
-                    magna aliqua.
-                </p>
+                <p>{this.props.pokemonDesc}</p>
             </div>
         ) 
     }
 }
 
-export default About;
+const mapStateToProps = (state) => {
+    return {
+        pokemonDesc: state.pokemonData.pokemonDesc
+    }
+}
+
+export default connect(mapStateToProps)(About);
