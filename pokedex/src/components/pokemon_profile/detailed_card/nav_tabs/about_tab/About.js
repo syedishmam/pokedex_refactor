649e6fdeb230dba53a8826878d6d59c93003a219
syedishmam <syedishmam17@gmail.com>
2020-03-23 21:21:44 -0400

    Add info boxes

--- pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/about_tab/About.js
+++ pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/about_tab/About.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import './styles/About.css';
+
 class About extends React.Component {
+
+    /*Possible About Additions
+        - Base Happiness
+        - Capture Rate
+        - Generation Released In
+        - Habitat
+        - Growth Rate
+    */
 
     render() {
         return (
-            <div>
+            <span>
                 <p id="description">{this.props.pokemonDesc}</p>
-            </div>
+                <div>
+                    <h3 id="measurementsHeading">Measurements</h3>
+                    <div class="floatingBox">
+                        <p id="height">Height: 5'7(170cm)</p>
+                        <p id="weight">Weight: 200bs(90.5kg)</p>
+                    </div>
+                    <h3 id="measurementsHeading">More Info</h3>
+                    <div class="floatingBox">
+                        <p id="height">Growth Rate: 45</p>
+                        <p id="weight">Capture Rate: 24</p>
+                    </div>
+                </div>
+            </span>
         ) 
     }
 
