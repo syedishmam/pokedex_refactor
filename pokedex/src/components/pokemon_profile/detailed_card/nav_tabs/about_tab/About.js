cacc24eb7656883dff10939a2f93ef005af0b08c
syedishmam <syedishmam17@gmail.com>
2020-03-26 18:48:45 -0400

    Convert stats to table format

--- pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/about_tab/About.js
+++ pokedex/src/components/pokemon_profile/detailed_card/nav_tabs/about_tab/About.js
@@ -5,28 +5,32 @@ import './styles/About.css';
 
 class About extends React.Component {
 
-    /*Possible About Additions
-        - Base Happiness
-        - Capture Rate
-        - Generation Released In
-        - Habitat
-        - Growth Rate
-    */
-
     render() {
         return (
             <span>
                 <p id="description">{this.props.pokemonDesc}</p>
                 <div>
                     <h3 id="measurementsHeading">Measurements</h3>
-                    <div class="floatingBox">
-                        <p id="height">Height: 5'7(170cm)</p>
-                        <p id="weight">Weight: 200bs(90.5kg)</p>
+                    <div className="floatingBox">
+                    <table>
+                            <tbody>
+                                <tr>
+                                    <td className="infoBoxStatCell">Height: 5'7(170cm)</td>
+                                    <td className="infoBoxStatCell">Weight: 200bs(90.5kg)</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <h3 id="measurementsHeading">More Info</h3>
-                    <div class="floatingBox">
-                        <p id="height">Growth Rate: 45</p>
-                        <p id="weight">Capture Rate: 24</p>
+                    <div className="floatingBox">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td className="infoBoxStatCell">Growth Rate: 50</td>
+                                    <td className="infoBoxStatCell">Capture Rate: 50</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </span>
