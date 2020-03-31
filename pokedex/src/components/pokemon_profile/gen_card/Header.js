559f7fbf5d0d982e3eca8aa49cdd4ef050e9909a
syedishmam <syedishmam17@gmail.com>
2020-03-31 16:39:22 -0400

    Bug Fix: About shown when profile clicked

--- pokedex/src/components/pokemon_profile/gen_card/Header.js
+++ pokedex/src/components/pokemon_profile/gen_card/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+
+import {displayAbout} from '../../../actions/index.js';
 
 import './styles/Header.css';
 
@@ -8,11 +10,11 @@ class Header extends React.Component {
     render() {
         return (
             <div className="headerContainer">
-                <Link className="backButton" to={'/catalog'}><i className="fas fa-arrow-left"></i></Link>
+                <Link className="backButton" to={'/catalog'}><i className="fas fa-arrow-left" onClick={() => this.props.displayAbout('about')}></i></Link>
                 <button className="favoriteButton"><i className="far fa-heart"></i></button>
             </div>
         )
     }
 }
 
-export default Header;
+export default connect(null, {displayAbout})(Header);
